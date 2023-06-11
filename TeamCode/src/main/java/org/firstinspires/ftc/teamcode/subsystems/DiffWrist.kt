63bea4fedf73cc6ea1f3b478a34f15d693d656b4
jackfetkovich <johnfetkovich1014@gmail.com>
2023-06-11 15:35:26 -0400

    added ability to read velocity from Axon CRServos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DiffWrist.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DiffWrist.kt
@@ -1,6 +1,34 @@
 package org.firstinspires.ftc.teamcode.subsystems
 
 import com.arcrobotics.ftclib.command.SubsystemBase
+import com.arcrobotics.ftclib.geometry.Rotation2d
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.DifferentialDriveKinematics
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.DifferentialDriveOdometry
+import com.qualcomm.robotcore.util.ElapsedTime
+import org.firstinspires.ftc.teamcode.utilities.AxonCRServo
+import java.util.concurrent.TimeUnit
 
-class DiffWrist : SubsystemBase() {
+class DiffWrist(private val leftServo: AxonCRServo, private val rightServo: AxonCRServo, val robot:Robot) : SubsystemBase() {
+    val kinematics: DifferentialDriveKinematics = DifferentialDriveKinematics(0.035)
+    val odometry: DifferentialDriveOdometry = DifferentialDriveOdometry(Rotation2d())
+    var translation = 0.0
+    var rotation = 0.0
+    var goalTranslation = 0.0
+    var goalRotation = 0.0
+
+
+    override fun periodic(){
+        /* Each servo will calculate its loop time by storing the degrees travelled
+        *  since the last time updateVelocity() was called */
+        leftServo.updateVelocity()
+        rightServo.updateVelocity()
+
+    }
+
+
+
+
+
+
+
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/AxonCRServo.kt
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode.utilities
+
+import com.qualcomm.robotcore.hardware.AnalogInput
+import com.qualcomm.robotcore.hardware.CRServoImplEx
+import com.qualcomm.robotcore.hardware.HardwareMap
+import com.qualcomm.robotcore.hardware.PwmControl.PwmRange
+import com.qualcomm.robotcore.util.ElapsedTime
+import java.util.concurrent.TimeUnit
+import kotlin.math.abs
+import kotlin.math.sign
+
+
+class AxonCRServo(
+    hw: HardwareMap,
+    servoName: String?,
+    analogName: String?,
+    min: Double,
+    max: Double
+) {
+    private val servo: CRServoImplEx
+    private val analogInput: AnalogInput
+    private val min: Double
+    private val max: Double
+    private val loopTimer = ElapsedTime()
+    private var lastLoopTime = 0.0
+    var velocity = 0.0 // in degrees per second
+        private set
+    private val analogOutput: Double
+        get() { return analogInput.voltage / 3.3 * 360}
+    private var lastAngleReading = 0.0
+
+
+    init {
+        servo = hw.get(CRServoImplEx::class.java, servoName)
+        analogInput = hw.get(AnalogInput::class.java, analogName)
+        servo.pwmRange = PwmRange(min, max)
+        this.min = min
+        this.max = max
+        lastAngleReading = analogOutput
+        loopTimer.reset()
+    }
+
+    fun setPower(power: Double) {
+        servo.power = power
+    }
+
+    // Must be called on every loop when using velocity control
+    fun updateVelocity(){
+        lastLoopTime = loopTimer.time(TimeUnit.SECONDS).toDouble()
+        val prevAngle = lastAngleReading
+        val currentAngle = analogOutput
+        var angleDelta = currentAngle - prevAngle
+
+        // TODO: Check value of 180 degrees
+        // Accounts for potentiometer wrap around
+        if(abs(angleDelta) > 180.0){
+            angleDelta += 360.0 * -sign(angleDelta)
+        }
+        velocity = angleDelta / lastLoopTime
+    }
+}
