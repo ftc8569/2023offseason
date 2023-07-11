90f44784a9cd271300c84eb22cf83c7c7db67646
Ben Caunt <bdcaunt@gmail.com>
2023-07-11 00:06:31 -0400

    SetLinearExtension

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/Extension/SetLinearExtension.kt
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.commands.Extension
+
+import com.arcrobotics.ftclib.command.CommandBase
+import com.arcrobotics.ftclib.command.CommandOpMode
+import com.qualcomm.robotcore.util.ElapsedTime
+import org.firstinspires.ftc.teamcode.subsystems.ExtensionLinkageSubsystem
+
+class SetLinearExtension(private val linkage : ExtensionLinkageSubsystem, private val length_in : Double) : CommandBase() {
+
+    init {
+        addRequirements(linkage)
+    }
+
+    private val timer = ElapsedTime()
+
+    private val duration = 0.35; // seconds
+
+
+    override fun initialize() {
+        linkage.actualPositionExtensionInches = length_in;
+        timer.reset()
+    }
+
+    override fun isFinished(): Boolean {
+        return timer.seconds() > duration
+    }
+
+}
